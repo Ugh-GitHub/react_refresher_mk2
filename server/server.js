edfc9fa7373878d35e40c95a1283deee7e11351f
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2021-11-08 15:35:54 -0600

    updated  react-router-dom to version w

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
-const portfolioRouter = require('./routes/portfolio.router.js');
-const contactRouter = require('./routes/contact.router.js');
+const bodyParser = require('body-parser');
+const movieRouter = require('./routes/movie.router.js');
+const genreRouter = require('./routes/genre.router.js');
+const detailRouter = require('./routes/detail.router.js');
+const movieGenre = require('./routes/movieGenre.router.js');
 const port = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(express.json()); // needed for angular requests
+app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-app.use('/api/portfolio', portfolioRouter);
-app.use('/api/contact', contactRouter);
+app.use('/api/movie', movieRouter);
+app.use('/api/genre', genreRouter);
+app.use('/api/detail', detailRouter);
+app.use('/api/moviegenre', movieGenre);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
